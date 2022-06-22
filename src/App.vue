74945c378b8e1050a5450276eb8278eb04c56815
txtyb <txtybbill@163.com>
2022-06-23 04:15:05 +0800

    Adapt to backend

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,68 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 import BrightnessChart from './components/BrightnessChart.vue'
+import { onBeforeMount, onMounted, ref, toRaw } from 'vue'
+const data = [
+  ['Time', 'Sales', 'Expenses'],
+  [new Date(1655606904467), 1000, 400],
+  [new Date(1655806907467), 1170, 460],
+  [new Date(1655906507467), 660, 1120],
+  [new Date(1655906907267), 1030, 540],
+];
+
+const options = {
+  // height: 600, 
+  // width: 800, 
+}
+
+const chartData = ref<Array<any>>([])
+
+function genDate(data:Array<any>) {
+  // console.log(data.length)
+  for (let i = 1; i < data.length; i++) {
+    // console.log(`i=${i}`)
+    data[i][0] = new Date(data[i][0])
+  }
+}
+
+async function refresh() {
+  let url = 'http://localhost:5000/api/getDevice?devEui=009569000000F51D&n=100'
+  try {let response = await fetch(url, {
+        method: "get",
+      });
+      chartData.value = await response.json()
+      console.log(`get data=${chartData.value}`)
+  } catch (error) {
+    console.log('get data failed', error)
+  }
+  console.log('refresh done!')
+  genDate(chartData.value)
+  console.log(`after gen=${chartData.value}`)
+}
+
+async function test() {
+  // console.log(chartData.value);
+}
+
+async function handleTest() {
+  await refresh();
+  await test();
+}
+onBeforeMount(() => {
+})
+
+onMounted(() => {
+  handleTest()
+})
+
 </script>
 
 <template>
   <div><img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   </div>
-  <div>
-    <BrightnessChart/>
+  <div class="chart">
+    <BrightnessChart :data="chartData" :options="options" />
   </div>
 </template>
 
@@ -23,4 +77,8 @@ import BrightnessChart from './components/BrightnessChart.vue'
   color: #2c3e50;
   margin-top: 60px;
 }
+/* .chart {
+  width: 100%;
+  height: 100%;
+} */
 </style>
--- src/components/BrightnessChart.vue
+++ src/components/BrightnessChart.vue
@@ -3,32 +3,41 @@ import { defineComponent, h } from 'vue';
 
 import { GChart } from 'vue-google-charts';
 
-const type = 'LineChart';
-
-const data = [
-    ['Year', 'Sales', 'Expenses'],
-    ['2004', 1000, 400],
-    ['2005', 1170, 460],
-    ['2006', 660, 1120],
-    ['2007', 1030, 540],
-];
-
 const options = {
-    title: 'Company Performance',
-    curveType: 'function',
-    legend: { position: 'bottom' },
-    width: 800,
-    height: 600,
+  title: '光照随时间变化图',
+  curveType: 'none',
+  explorer: {}, 
+  legend: { position: 'top' },
+  width: 1600,
+  height: 1200,
 };
 
-const chart = h(GChart, {
-    data,
-    options,
-    type,
-});
+const props = defineProps({
+  data: {
+    type: Array<any>,
+    required: true,
+  },
+  options: {
+    type: Object,
+  },
+})
+
+const optionsMerged = Object.assign({}, options, props.options)
+
+// const data = ref<Array<any>[]>(props.data)
+// console.log(data)
+
+// const chart = h(GChart, {
+//     data,
+//     options,
+//     type,
+// });
+
+// console.log(chart)
 
 </script>
 
 <template>
-<chart/>
+  <!-- <chart/> -->
+  <GChart :settings="{ 'language': 'zh' }" type="LineChart" :data="props.data" :options="optionsMerged" />
 </template>
